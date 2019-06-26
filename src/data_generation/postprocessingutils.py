70af51b1c0ec692f15b69f8673210c0081faaf01
papajan3000 <janpaulus95@web.de>
2019-06-26 12:08:34 +0200

    add: removes nonlatin phrases

--- src/data_generation/postprocessingutils.py
+++ src/data_generation/postprocessingutils.py
@@ -3,18 +3,25 @@ import re
 import pandas as pd
 from nltk import word_tokenize
 
-
 ### Helper functions ###
 
-def load_dic(path="data/german.dic"):
+def load_dic(path="data/german.dic", encoding="ISO-8859-1"):
     """Returns a set with german dictionary words.
     
     Args:
         path (string): Path to the dic-file.
+        encoding (string): Name of the Encoding.
     Returns:
         Set with german dictionary words.    
     """
-    with open(path, "r", encoding="utf-8", errors="ignore") as dic:
+    #detects the encoding of the file if the encoding is unknown
+    if encoding == "unknown":
+        import chardet
+        rawdata = open(path, 'rb').read()
+        result = chardet.detect(rawdata)
+        encoding = result['encoding']
+    
+    with open(path, "r", encoding=encoding, errors="ignore") as dic: 
         germandic = [word[:-1] for word in dic]
     return set(germandic)
 
@@ -23,9 +30,18 @@ def hasNumbers(string):
     """Returns True if the string contains a number."""
     return any(char.isdigit() for char in string)
 
-#TODO: docstring
+
 def find_foreign_phrase(string, dic):
-    """ """
+    """Finds the foreign phrase in front of the parentheses, removes everything
+        before the phrase and returns the leftover.
+    
+    Args:
+        string (string): Sentence or phase as string.
+        dic (set): Set with dictionary words as strings.
+    Returns:
+        String without the words before the foreign phrase in front of the
+        parantheses.
+    """
     foreign_words = []
     for idx, word in enumerate(word_tokenize(string)):
         if word in dic or word.capitalize() in dic or word.lower() in dic:
@@ -45,23 +61,63 @@ def find_foreign_phrase(string, dic):
         
     return string[string.find(last_phrase_word):]
 
-
 ### postprocessing functions ###
     
-def only_german_translation(text, dic):
-    """Removes foreign terms or phrases when their german translation is given in parantheses.
+def remove_nonlatin(text):
+    """Removes non-latin characters and replaces some non-latin characters 
+        with latin equivalents.
+    
+    Args:
+        text (string): String with non-latin substrings.
+    Returns:
+        String with removed non-latin characters.        
+    """
+    new_text = re.sub('„', '"', text)
+    new_text = re.sub('“', '"', new_text)
+    new_text = re.sub('”', '"', new_text)
+    new_text = re.sub('‚', "'", new_text)
+    new_text = re.sub('‘', "'", new_text)
+    new_text = re.sub('’', "'", new_text)
+    new_text = re.sub('–', '-', new_text)
+    new_text = re.sub('‒', '-', new_text)
+    new_text = re.sub('−', '-', new_text)
+    nonlatin = re.compile('[^\x00-\x7F\x80-\xFF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF\u2020]')
+    new_text = re.sub(nonlatin,  "", new_text)
+    
+    return new_text
+
+def replace_umlauts(text):
+    """Replaces german umlauts in a string.
+    
+    Args:
+        text (string): String with umlaut-substrings.
+    Returns:
+        String with replaced umlaut-substrings
+    """
+    replaced_text = text.replace('ä', 'ae')
+    replaced_text = replaced_text.replace('ö', 'oe')
+    replaced_text = replaced_text.replace('ü', 'ue')
+    replaced_text = replaced_text.replace('Ä', 'Ae')
+    replaced_text = replaced_text.replace('Ö', 'Oe')
+    replaced_text = replaced_text.replace('Ü', 'Ue')
+    replaced_text = replaced_text.replace('ß', 'ss')
+    
+    return replaced_text
+
+def replace_german_translation(text, dic):
+    """Removes foreign terms or phrases when their german translation is given 
+        in parantheses.
     
     Args:
         text (string): String of a Wikipedia article.
         dic (set): Set with dictionary words as strings.
     Returns:
-        String without the foreign terms or phrases but their german translation instead.
+        String without the foreign terms or phrases 
+        but their german translation instead.
     """
-    #TODO: erst muss removes non latin gemacht werden
-    
-    # matches 15 words or quotation before parantheses
+    # matches 15 words or quotation in front of the parantheses
     regex = re.compile("((?:(\w+\s+|\w+')){1,15}\(((d|D)eutsch:|zu (d|D)eutsch).*?\)|\"([^\"]*)\" \(((d|D)eutsch:|zu (d|D)eutsch):.*?\))")
-    original_and_translation = regex.search(text)
+    original_and_translation = regex.search(remove_nonlatin(text))
     
     if original_and_translation != None:
         original = re.search("^.*?\(", original_and_translation.group()).group()[:-1]
@@ -76,14 +132,20 @@ def only_german_translation(text, dic):
         return text[:text.find(original)] + translation + text[text.find(paranthesis.group()) + len(paranthesis.group()):]
     else:
         return text
-    
+
+
 #%%
     
 dic = load_dic()
 s = 'An mehreren Stellen what is it im Film ist der Satz Who watches the Watchmen (zu deutsch: "Wer überwacht die Wächter?") zu sehen.'
 
 
+print(find_foreign_phrase(s, dic))
+print("\n")
 print(only_german_translation(s, dic))
+print("\n")
+s = "sddsd † “ sddsd dsdd sd"
+print(remove_nonlatin(s))
 
 
 
